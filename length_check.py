e6a55012c9d105f3fc42bfd632ab0f5c755233ac
hesamsaeidi <66099111+hesamsaeidi@users.noreply.github.com>
2021-02-24 12:14:24 -0600

    Add files via upload
    prepare files for AutoPick in each network folder

--- length_check.py
+++ length_check.py
@@ -1,64 +1,119 @@
-# This script must be run before filtering.
-# use `$>python3 4-1_length_check.py` in the network directory
-# This code checks if events in a network has equal length
-# or not, as a prepration for rotating to gc
-# If the length of E and N are not equal, the data
-# will trimmed to the length of the shorter one
-# It needs 2 lists for processing; eventList & stationList
-# The scripts flush the report into a log file named `length_check.log`
-
-
-import subprocess
+# This scripts check if all the streams in one directory
+# which are related to one seismic event recorded in
+# different stations have the same npts or not?
+# If the differnce is less that 200 npts, the script
+# cut the long streams. But if there are any streams
+# with less than 200 npts than the longest in the event
+# all of them will be renamed with a ".short" appended
+### IMPORTANT ###
+# this code works with ?HZ only files
 import os
 import obspy
+import warnings
+import time
+
+
+def call_trim(ev_dir):
+    # this function should be called for one event with
+    # different values for npts in their header
+    # Then it will iterate through each stream and cut it
+    # from its beginning time up to the value of the shortest
+    # npts in the event
+    generic_path = ev_dir+"/"+"*.?HZf"
+
+    z_comps = obspy.read(generic_path, debug_headers=True)
+    shortest_npts = z_comps[0].stats.npts
+    longest_npts = z_comps[0].stats.npts
+    smpl_r = z_comps[0].stats.sampling_rate
+    samp_rate_warn = False
+    for st_i in range(len(z_comps)):
+        if z_comps[st_i].stats.npts < shortest_npts:
+            shortest_npts = z_comps[st_i].stats.npts
+
+    for st_i in range(len(z_comps)):
+        if z_comps[st_i].stats.npts > longest_npts:
+            longest_npts = z_comps[st_i].stats.npts
+    for st_i in range(len(z_comps)):
+        if z_comps[st_i].stats.sampling_rate != smpl_r:
+            samp_rate_warn = True
+            stat_name = z_comps[st_i].stats.station
+            warnings.warn(f'Different sampling rates of {stat_name} in {generic_path}!')
+
+    if longest_npts != shortest_npts and not samp_rate_warn:
+
+
+        for st_i in range(len(z_comps)):
+
+            t1 = z_comps[st_i].stats.starttime
+            smpl_r = z_comps[st_i].stats.sampling_rate
+            t2 = t1 + (shortest_npts/smpl_r) - 1/smpl_r
+            stat_name = z_comps[st_i].stats.station
+            stat_ch = z_comps[st_i].stats.channel
+            str_path = ev_dir + "/" + stat_name + ".BHZf"
+            z_comps[st_i].trim(t1, t2)
+            time.sleep(0.1)
+            z_comps[st_i].write(str_path, format='SAC')
+            time.sleep(0.1)
+    elif samp_rate_warn:
+        warnings.warn('check sampling rates and rerun the code!')
+        samp_rate_warn = False
+    else:
+        print('no trimming needed!!!! \n')
+
+
+
+def cast_out_unsuited_streams(stat_name, stat_ch, ev_dir):
+    # apped ".unsuited" at the end of the filenames without proper
+    # criteria, either less npts or no predicted time to cast them
+    # out of the auto picking procedure
+    str_path = ev_dir + "/" + stat_name + ".BHZf"
+    new_str_path = str_path + ".unsuited"
+    os.rename(str_path, new_str_path)
+    print(str_path)
+    print(new_str_path)
+    warnings.warn(f'\nOne or more files have been renamed in {ev_dir}!')
+
+
 
 # Define absolute directory of the network path
 try:
-    home_dir = '/Users/hesam/AFRICA/1C'
+    # home_dir = '/Users/hesam/Documents/MCCC/AFRICA/test'
+    home_dir = os.getcwd()
     event_list = []
     with open("eventList", 'r') as el:
         for line in el:
             event_list.append(line.strip())
 
-
-    station_list = []
-    with open("stationList", 'r') as sl:
-        for line in sl:
-            if not line:
-                pass
-            else:
-                station_list.append(line.strip())
 except FileNotFoundError:
     print("eventList and/or stationList is not created")
 
-# Error log
-file = open("length_check.log", "w")
 for event in event_list:
-    for sta in station_list:
-        n_comp = home_dir+"/"+event+"/"+sta+ ".BHN"
-        e_comp = home_dir+"/"+event+"/"+sta+ ".BHE"
-        if os.path.isfile(e_comp) & os.path.isfile(n_comp):
-            st_N = obspy.read(n_comp, debug_headers=True)
-            st_E = obspy.read(e_comp, debug_headers=True)
-            if st_N[0].stats.npts == st_E[0].stats.npts:
-                pass
+    ev_dir = home_dir+"/"+event
+    generic_path = ev_dir+"/"+"*.?HZf"
+    try:
+        z_comps = obspy.read(generic_path, debug_headers=True)
 
-            else:
-                file.write(f">>> The {sta} has different length for E and N component\n")
-                if st_N[0].stats.npts < st_E[0].stats.npts:
-                    t1 = st_N[0].stats.starttime
-                    t2 = st_N[0].stats.endtime
-                    st_E[0].trim(t1, t2)
-                    st_E.write(e_comp, format='SAC')
-
+        for st_i in range(len(z_comps)):
+            if z_comps[st_i].stats.sac.t2 != -12345.0:
+                tB = z_comps[st_i].stats.sac.b #begin time
+                tE = z_comps[st_i].stats.sac.e #end time
+                t2 = z_comps[st_i].stats.sac.t2 #tuap predicted time
+                smpl_r = z_comps[st_i].stats.sampling_rate
+                if t2 - 25 > tB and t2 + 25 < tE:
+                    pass
                 else:
-                    t1 = st_E[0].stats.starttime
-                    t2 = st_E[0].stats.endtime
-                    st_N[0].trim(t1, t2)
-                    st_N.write(n_comp, format='SAC')
+                    stat_name = z_comps[st_i].stats.station
+                    stat_ch = z_comps[st_i].stats.channel
+                    cast_out_unsuited_streams(stat_name, stat_ch, ev_dir)
+            else:
+                stat_name = z_comps[st_i].stats.station
+                stat_ch = z_comps[st_i].stats.channel
+                cast_out_unsuited_streams(stat_name, stat_ch, ev_dir)
 
-        else:
-            file.write(f"--- The {sta} does not have E or N component for {event}.\n")
+    except Exception as e:
+        print(e)
 
-
-file.close()
+    try:
+        call_trim(ev_dir)
+    except Exception as e:
+        print(e)
